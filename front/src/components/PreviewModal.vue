26a1160d85f059c7e628586b75f64547c8a656a4
yjh <929323982@qq.com>
2026-05-20 12:32:29 +0800

    style(PreviewModal): 移除多余的分片页码提示文字
    删除了PDF预览弹窗中冗余的"分片所在: 第 {{ props.pageNumber }} 页"提示内容

--- front/src/components/PreviewModal.vue
+++ front/src/components/PreviewModal.vue
@@ -277,7 +277,6 @@ function onClose() {
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>
           {{ fileName }}
         </span>
-        <span class="preview-page" v-if="isPdf && props.pageNumber">分片所在: 第 {{ props.pageNumber }} 页</span>
         <button class="preview-close" @click="onClose">&times;</button>
       </div>
 
